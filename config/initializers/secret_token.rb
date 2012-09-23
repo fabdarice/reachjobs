a3138743f7a50b09377e2bb3d958f87f76fddbd4
Fabrice Cheng <fabrice.cheng@gmail.com>
2012-09-23 22:30:10 +0200

    + Initialize project "ReachJobs"
    Add Devise GEM

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reachjobs::Application.config.secret_token = '77420ad5a25143dd9a6c450aa130047d46990a41f5c95634dd15dcd1bf1abf714271a8e1abe25c6a0938b20f6b128f0b476e038ca5a54eb5c765fb397f425d89'
