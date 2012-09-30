2c2e7fc9e8757970f04e237f9dba22bc1d2a9dbc
Fabrice Cheng <fabrice.cheng@gmail.com>
2012-09-30 14:12:29 +0200

    + Integration of Register/Sign Up Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  #Use to display error message 
+  def display_error(record, attr_name)
+    if record.errors[attr_name].present?
+      record.errors[attr_name].first
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable :validatable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :lastname, :firstname
+  validates :lastname, :firstname,:password, :email, :password_confirmation, presence: true
+
+  validates :email, uniqueness: true
+  validates :password, :length => {:minimum => 6, :too_short => "6 characters minimum"}
+  validates :password, :confirmation => true
   # attr_accessible :title, :body
 end
