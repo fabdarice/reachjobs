c1133edf6049ce3811aa0b698a0af65894ad0817
Fabrice Cheng <fabrice.cheng@gmail.com>
2012-11-11 00:36:56 +0100

    Add default order for skills as CREATED_AT ASC

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,7 @@
 class Skill < ActiveRecord::Base
+
+  default_scope order('created_at ASC')
+
   attr_accessible :skill_name
   has_and_belongs_to_many :skillcategories, :uniq => true
   has_and_belongs_to_many :workexperiences, :uniq => true
